b8d2407e8539b612d83e13844797effce472b9d3
tiagobani <tiago.bani@yahoo.com.br>
2020-09-03 21:13:54 -0300

    [feat]: add handling errors request

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,11 @@
 import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
-import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
-import { Title, Form, Repositories } from './styles';
-import Repository from '../Repository/index';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
   full_name: string;
@@ -18,15 +18,27 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
-    const { data } = await api.get<Repository>(`repos/${newRepo}`);
-    setRepositories([...repositories, data]);
-    setNewRepo('');
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const { data } = await api.get<Repository>(`repos/${newRepo}`);
+      setRepositories([...repositories, data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
   }
 
   return (
@@ -34,14 +46,16 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
-          placeholder="Digite o nome do repositório"
+          placeholder="Digite o autor/nome do repositório"
           value={newRepo}
           onChange={event => setNewRepo(event.target.value)}
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories &&
